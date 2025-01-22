fe1cec283f4b87b63d2db17b7aa54594cf4c6003
Paolabustos0510 <pabustos@unal.edu.co>
2025-01-21 19:05:08 -0500

    Resultado Laboratorio 05 Descriptiva

--- homework/homework.py
+++ homework/homework.py
@@ -49,9 +49,67 @@ def clean_campaign_data():
 
 
     """
+    import pandas as pd
+    import os
+    import shutil
 
-    return
+    def open_clean(path):
+        # Leer todos los archivos en la carpeta de entrada
+        files = os.listdir(path)
+        data = []
+        for file in files:
+            full_path = os.path.join(path, file)
+            data.append(full_path)
+        final_df = pd.DataFrame()
 
+        
+        for i in data:
+            df = pd.read_csv(i, index_col=False, compression='zip')    
+            df['job'] = df['job'].str.replace('.', '').str.replace('-', '_')
+            df['education'] = df['education'].str.replace('.', '_', regex=False).replace('unknown', pd.NA)
+            df['credit_default'] = df['credit_default'].apply(lambda x: 1 if x == 'yes' else 0)
+            df['mortgage'] = df['mortgage'].apply(lambda x: 1 if x == 'yes' else 0)
+            df['previous_outcome'] = df['previous_outcome'].apply(lambda x: 1 if x == 'success' else 0)
+            df['campaign_outcome'] = df['campaign_outcome'].apply(lambda x: 1 if x == 'yes' else 0)
+            
+            month_dict = {'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5, 'jun': 6,'jul': 7, 'aug': 8, 'sep': 9, 'oct': 10, 'nov': 11, 'dec': 12}
+            df['month'] = df['month'].str.lower().map(month_dict).fillna(df['month'])
+            df['last_contact_date'] = "2022-" + df['month'].astype(str).str.zfill(2) + "-" + df['day'].astype(str).str.zfill(2)
+            
+            final_df = pd.concat([final_df, df], ignore_index=True)
+        
+        return final_df
+
+    def divide_save(df):
+
+        client = df[['client_id', 'age', 'job', 'marital', 'education', 'credit_default', 'mortgage']]
+        campaign = df[['client_id', 'number_contacts', 'contact_duration', 'previous_campaign_contacts', 
+                    'previous_outcome', 'campaign_outcome', 'last_contact_date']]
+        economics = df[['client_id', 'cons_price_idx', 'euribor_three_months']]
+        
+        files = {
+            'client': client,
+            'campaign': campaign,
+            'economics': economics
+        }
+        output_folder = 'files/output/'
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+            print(f"La carpeta '{output_folder}' fue creada.")
+        for name, data in files.items():
+            path = f'files/output/{name}.csv'
+            data.to_csv(path, index=False)
+
+
+
+
+    # Cargar los datos y dividirlos
+    df = open_clean('files/input/')
+    # Guardar los datos
+    divide_save(df)
 
 if __name__ == "__main__":
     clean_campaign_data()
+
+
+
